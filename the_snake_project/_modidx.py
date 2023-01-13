db09628f5c5dfdd5ebc2a692d20d0c41bf871876
Jimmie Munyi <jimmiemunyi@gmail.com>
2023-01-13 19:17:09 +0300

    baseline training 10 classes

--- the_snake_project/_modidx.py
+++ the_snake_project/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://jimmiemunyi.github.io',
                 'git_url': 'https://github.com/jimmiemunyi/the-snake-project',
                 'lib_path': 'the_snake_project'},
-  'syms': {'the_snake_project.core': {'the_snake_project.core.get_species': ('core.html#get_species', 'the_snake_project/core.py')}}}
+  'syms': { 'the_snake_project.core': { 'the_snake_project.core.get_dls': ('core.html#get_dls', 'the_snake_project/core.py'),
+                                        'the_snake_project.core.get_species': ('core.html#get_species', 'the_snake_project/core.py'),
+                                        'the_snake_project.core.get_x': ('core.html#get_x', 'the_snake_project/core.py'),
+                                        'the_snake_project.core.get_y_gns': ('core.html#get_y_gns', 'the_snake_project/core.py')}}}
